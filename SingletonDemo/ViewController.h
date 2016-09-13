5f580452f4d813fb9491f49f30c99c829d6aaa11
CharlesYaoxin <Charlesyaoxin@163.com>
2016-09-13 12:13:07 +0800

    Initial Commit

---
+++ SingletonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingletonDemo
+//
+//  Created by Charles on 16/9/13.
+//  Copyright © 2016年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SingletonDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SingletonDemo
+//
+//  Created by Charles on 16/9/13.
+//  Copyright © 2016年 Charles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
